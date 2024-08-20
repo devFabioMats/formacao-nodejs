1b97957d95c71685f0217afc3aa71b264f33e894
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-20 18:37:40 -0300

    Create index.js

---
+++ 03-projeto-mario-kart/src/index.js
@@ -0,0 +1,40 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+//funcao para rolar dados
+//async = funcao que nao é executada imediatamente
+async function rollDice() {
+    //floor = arredonda para baixo e random = gera um número aleatório 
+    return Math.floor(Math.random() * 6) + 1; 
+};
+
+/* 
+// chama a função main para mostrar no terminal
+async function main() {
+    console.log("hello");
+};
+main();
+//-----------------------------------------------------
+// funcao auto-invocável, sem precisar "chamá-la"
+(async function main() {
+    console.log("hello");
+})();
+*/
+
+(async function main() {
+    // win + . para abrir emojis
+    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... \n`);
+})();
