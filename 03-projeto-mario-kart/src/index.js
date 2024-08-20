43d03d3f9f2f5d362baeacb50fefc15fb5495d1a
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-20 20:41:24 -0300

    Otimizando os IF (IF's Ternários)

--- 03-projeto-mario-kart/src/index.js
+++ 03-projeto-mario-kart/src/index.js
@@ -73,6 +73,7 @@ async function playRaceEngine(character1, character2) {
             await logRollresult(character1.NOME, "velocidade", diceResult1, character1.VELOCIDADE);
             await logRollresult(character2.NOME, "velocidade", diceResult2, character2.VELOCIDADE);
         }
+
         if (block === "CURVA") {
             totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
             totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
@@ -80,26 +81,44 @@ async function playRaceEngine(character1, character2) {
             await logRollresult(character1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
             await logRollresult(character2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
         }
+
         if (block === "CONFRONTO") {
             let powerResult1 = diceResult1 + character1.PODER;
             let powerResult2 = diceResult2 + character2.PODER;
 
+            console.log(`${character1.NOME} confrontou com ${character2.NOME}! 🥊`);
+
             await logRollresult(character1.NOME, "poder", diceResult1, character1.PODER);
             await logRollresult(character2.NOME, "poder", diceResult2, character2.PODER);
+
+            /*
+            if (powerResult1 > powerResult2) {
+                if(character2.PONTOS > 0) {
+                    character2.PONTOS--;
+                }
+            }
+            */
+            //OTIMIZANDO IF's
+            //se o poder do personagem 1 for maior que o poder do personagem 2 e os pontos do personagem 2 forem maiores que 0, então o personagem 2 perde 1 ponto
+            character2.PONTOS -= powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 : 0;
+            //se o poder do personagem 2 for maior que o poder do personagem 1 e os pontos do personagem 1 forem maiores que 0, então o personagem 1 perde 1 ponto
+            character1.PONTOS -= powerResult2 > powerResult1 && character1.PONTOS > 0 ? 1 : 0;
+            //se o poder do personagem 1 for igual ao poder do personagem 2, então ninguém perde pontos
+            console.log(powerResult2 == powerResult1 ? "Confronto empatado! Nenhum ponto foi perdido!" : "");
         }
 
         //verificando o vencedor
-        if(totalTestSkill1 > totalTestSkill2) {
-            console.log(`\n${character1.NOME} marcou 1 ponto!`)
+        if (totalTestSkill1 > totalTestSkill2) {
+            console.log(`\n${character1.NOME} marcou 1 ponto!`);
             character1.PONTOS++;
         } else if (totalTestSkill2 > totalTestSkill1) {
-            console.log(`\n${character2.NOME} marcou 1 ponto!`)
+            console.log(`\n${character2.NOME} marcou 1 ponto!`);
             character2.PONTOS++;
         } else if (totalTestSkill1 == totalTestSkill2) {
-            console.log(`\nEmpate! Ninguém marcou pontos!`)
+            console.log(`\nEmpate! Ninguém marcou pontos!`);
         }
 
-        console.log("-------------------------------------")
+        console.log("-------------------------------------");
     }
 }
 
