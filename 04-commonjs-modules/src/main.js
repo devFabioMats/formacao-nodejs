47bccefc84b0941e6f4f8aef45df626e276e62e9
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-21 16:30:53 -0300

    export consts

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,13 +1,16 @@
 // requerendo/importando o arquivo products
 const p = require("./services/products");
+const config = require("./services/config");
 
 async function main(){
     console.log("Carrinho Compras");
 
-    p.getFullName("408", "mousepad");
-    p.getFullName("508", "monitor");
+    // p.getFullName("408", "mousepad");
+    // p.getFullName("508", "monitor");
+    // p.getProductLabel("mousepad");
 
-    p.getProductLabel("mousepad");
+    console.log(config.production);
+    console.log(config.client);
 
 }
 
