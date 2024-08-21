9ae2b598ee4ca9bb038917bfbb623712b12361a8
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-21 19:26:15 -0300

    direct export

--- 05-esm-modules/src/index.js
+++ 05-esm-modules/src/index.js
@@ -1,4 +1,6 @@
 // const database = require("./utils/database.js");
 import { disconnectFromDatabase, databaseType } from "./utils/database.js";
+import { getDataFromApi } from "./utils/api.js";
 
+getDataFromApi();
 disconnectFromDatabase("my-database");
---
+++ 05-esm-modules/src/utils/api.js
@@ -0,0 +1,15 @@
+const key = {
+  value: "2313jklfds",
+  perimission: "admin",
+};
+
+//direct export
+export const getDataFromApi = () => {
+  console.log("Dados da api sendo buscados...");
+};
+
+// exports.getDataFromApi = () => {
+//     console.log("Dados da api sendo buscados...");
+// }
+
+//export default key;
