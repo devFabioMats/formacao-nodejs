85ffd9e3ca365a47d3806693429e1c65740df1f7
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-21 16:14:56 -0300

    importando modulo

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,7 +1,11 @@
-async function main(){
-    console.log("ola meu nome é system")
+// requerendo/importando o arquivo products
+const p = require("./services/products");
 
-    
+async function main(){
+    console.log("Carrinho Compras");
+    p.getFullName("408", "mousepad");
+    p.getFullName("508", "monitor");
+
 }
 
 main();
--- 04-commonjs-modules/src/services/products.js
+++ 04-commonjs-modules/src/services/products.js
@@ -1,6 +1,7 @@
 // todas as funções que lidam com produto
 async function getFullName(codeId, productName) {
-    return codeId + "--" + productName;
+    console.log("\n");
+    console.log("product: " + codeId + "--" + productName);
 }
 
 // mostrando ao js quais funções eu quero exportar para fora
