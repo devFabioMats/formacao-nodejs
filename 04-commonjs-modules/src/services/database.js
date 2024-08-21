e84493f88be9c2f80afa1a88f17ba32f5767234d
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-21 16:43:58 -0300

    export default

---
+++ 04-commonjs-modules/src/services/database.js
@@ -0,0 +1,18 @@
+// export default
+
+// async function connecToDatabase(dataName){
+//     console.log("Conectando ao banco de dados: " + dataName);
+// }
+
+exports.connecToDatabase = (dataName) =>{
+    console.log("Conectando ao banco de dados: " + dataName);
+}
+
+// async function disconnectDataBase(){
+//     // lógica
+//     console.log("Desconectando do banco de dados");
+// }
+
+exports.disconnectDataBase = () => {
+    console.log("Desconectando do banco de dados");
+}
