9c25f431e5b545d7a7f9cc2b0d12c5aef72890f8
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-22 15:38:33 -0300

    Assinaturas cart.s

---
+++ 06-shopee-cart/src/services/cart.js
@@ -0,0 +1,22 @@
+// quais ações meu carrinho pode fazer
+
+// CASOS DE USO
+// -> adicionar item no carrinho
+async function addItem(userCart){
+
+}
+
+// -> deletar item do carrinho
+async function deleteItem(userCart, name){
+
+}
+
+// -> remover um item - diminui um item
+async function removeItem(userCart, index){
+
+}
+
+// -> calcular total do carrinho
+async function calculateTotal(userCart){
+
+}
