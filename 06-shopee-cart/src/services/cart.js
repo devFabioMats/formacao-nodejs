d57d5b35bac467cb53c5ae5769fa8f87ba015053
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-08-22 15:58:14 -0300

    update cart add e cart subtotal

--- 06-shopee-cart/src/services/cart.js
+++ 06-shopee-cart/src/services/cart.js
@@ -1,9 +1,15 @@
 // quais ações meu carrinho pode fazer
 
 // CASOS DE USO
-// -> adicionar item no carrinho
+// ✅ -> adicionar item no carrinho
 async function addItem(userCart, item) {
   userCart.push(item);
+}
+
+// -> calcular total do carrinho
+async function calculateTotal(userCart) {
+  // valor anterior (total) + valor de outro item (item.subtotal()) = novo total
+  return userCart.reduce((total, item) => total + item.subtotal(), 0);
 }
 
 // -> deletar item do carrinho
@@ -11,6 +17,3 @@ async function deleteItem(userCart, name) {}
 
 // -> remover um item - diminui um item
 async function removeItem(userCart, index) {}
-
-// -> calcular total do carrinho
-async function calculateTotal(userCart) {}
